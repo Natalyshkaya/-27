2d341e003ef27eb0b98386d3dd00765208b7dda6
Nataly <naty.sh@mail.ru>
2022-11-27 14:05:01 +0500

    add programm N27

---
+++ задача27.2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задча 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+// Для решения задания использование цикла for является обязательным условием
+
+int Summa(int number)
+{
+    string StrNumber = number.ToString();
+    int sum = 0;
+    for (int i = 0; i < StrNumber.Length; i++)
+    {
+        sum = sum + Convert.ToInt32(StrNumber[i].ToString());
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    Console.WriteLine("Введено некорректное число");
+    return;
+}
+else
+{
+    Console.WriteLine($"Сумма цифр в числе {number}={Summa(number)}");
+}
